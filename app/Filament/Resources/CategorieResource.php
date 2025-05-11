d00cdbfe14e1720954446e2857297ed7a18f0191
mamangesjeruksiboy <adryanryan354@gmail.com>
2025-05-12 01:55:31 +0700

    icon

--- app/Filament/Resources/CategorieResource.php
+++ app/Filament/Resources/CategorieResource.php
@@ -17,7 +17,7 @@ class CategorieResource extends Resource
 {
     protected static ?string $model = Categorie::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-tag';
 
     protected static ?string $navigationGroup = 'Berita';
 
